a0231fb92ec7d130a2a16e3ffe3e87edd4ecfa10
Dmitriymgtu <dmitriykhotin@mail.ru>
2021-02-04 13:41:32 +0300

    end auth

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
 
     const client_id = '2314c60c89c345ddadf5827a04c3fc76'
     const response_type = 'token'
-    const redirect_uri = 'https://localhost:3000'
+    const redirect_uri = 'http://localhost:3000'
     const scope = 'user-read-email user-read-private'
     window.location.href = `https://accounts.spotify.com/authorize?client_id=${client_id}&redirect_uri=${encodeURIComponent(redirect_uri)}&scope=${encodeURIComponent(scope)}&response_type=${response_type}`
   }
